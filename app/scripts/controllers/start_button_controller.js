c3b1ad476e71376e9e9ccda304831bcbe42c3219
zhangkehbg <562380406@qq.com>
2014-07-23 11:28:10 +0800

    实现只要有活动在报名，其他活动页面上的开始按钮为不可点击的灰色

--- app/scripts/controllers/start_button_controller.js
+++ app/scripts/controllers/start_button_controller.js
@@ -9,6 +9,11 @@
             else{
                 $scope.startbutton=true;
             }
+            if(localStorage.getItem("started_activity")!=[]&&localStorage.getItem("started_activity")!=localStorage.getItem("book_partyname"))
+            {
+                $scope.button_able=true;
+            }
+
 //            $scope.startbutton=localStorage.getItem(["activity_start"]||[true]);
 //            localStorage.setItem("activity_start",$scope.startbutton);
             $scope.start=function()
@@ -24,6 +29,7 @@
                 {
                     $scope.startbutton=true;
                     localStorage.setItem("activity_start","activity_over");
+                    localStorage.setItem("started_activity","");
                 }
                 else{
                     return false;
